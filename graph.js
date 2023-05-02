088d257f766f2afd151761b1214d8684a1000eed
Anthony Bui <anthonyb50@gmail.com>
2023-05-01 22:20:16 -0400

    Completed graph class

--- graph.js
+++ graph.js
@@ -1,21 +1,41 @@
 class Graph {
   constructor() {
-    this.adjancencyList = {};
+    this.adjacencyList = {};
   }
   addVertex (vertex) {
-    if (!this.adjancencyList[vertex]) {
-      this.adjancencyList[vertex] = new Set();
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = new Set();
     }
   }
   addEdge(fromVertex, toVertex) {
-    if (!this.adjancencyList[fromVertex]) {
+    if (!this.adjacencyList[fromVertex]) {
       this.addVertex(fromVertex);
     }
-    if (!this.adjancencyList[toVertex]) {
+    if (!this.adjacencyList[toVertex]) {
       this.addVertex(toVertex);
     }
-    this.adjancencyList[fromVertex].add(toVertex);
-    this.adjancencyList[toVertex].add(fromVertex);
+    this.adjacencyList[fromVertex].add(toVertex);
+    this.adjacencyList[toVertex].add(fromVertex);
+  }
+  display() {
+    for (let vertex in this.adjacencyList) {
+      console.log(`${vertex} -> ${[...this.adjacencyList[vertex]]}`)
+    }
+  }
+  hasEdge(vertex1, vertex2) {
+    return (this.adjacencyList[vertex1].has(vertex2) && this.adjacencyList[vertex2].has(vertex1))
+  }
+  removeEdge(fromVertex, toVertex) {
+    this.adjacencyList[fromVertex].delete(toVertex);
+    this.adjacencyList[toVertex].delete(fromVertex);
+  }
+  removeVertex(vertex) {
+    if (this.adjacencyList[vertex]) {
+      for (let adjacentVertex of this.adjacencyList[vertex]) {
+        this.removeEdge(adjacentVertex, vertex);
+      }
+    }
+    delete this.adjacencyList[vertex];
   }
 }
 
@@ -30,4 +50,15 @@ console.log(graph);
 
 graph.addEdge('A', 'C');
 graph.addEdge('D','A');
+graph.addEdge('B','A');
 console.log(graph);
+
+// graph.display();
+
+console.log(graph.hasEdge('A', 'B'));
+console.log(graph.hasEdge('A', 'C'));
+
+graph.removeEdge('A','C');
+// graph.display();
+graph.removeVertex('A');
+graph.display();
